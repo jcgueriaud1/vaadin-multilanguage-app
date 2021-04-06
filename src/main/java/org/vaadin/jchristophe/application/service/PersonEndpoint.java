a19a24053abcb9b6484702081063fb8ca5bc507e
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2021-04-06 14:43:43 +0300

    Add a custom validator only on the server side + remove the manual check since it's done automatically

--- src/main/java/org/vaadin/jchristophe/application/service/PersonEndpoint.java
+++ src/main/java/org/vaadin/jchristophe/application/service/PersonEndpoint.java
@@ -1,18 +1,8 @@
 package org.vaadin.jchristophe.application.service;
 
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.server.connect.Endpoint;
 import com.vaadin.flow.server.connect.auth.AnonymousAllowed;
-import com.vaadin.flow.spring.annotation.SpringComponent;
 import org.vaadin.jchristophe.application.data.Person;
-
-import javax.annotation.PostConstruct;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.util.Optional;
-import java.util.Set;
 
 /**
  * @author jcgueriaud
